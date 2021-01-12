068c39390fea418cbd6598e28bb5107e371ca1e7
bmburakmert <bmburakmert@gmail.com>
2021-01-12 12:36:44 +0000

    add Navi.js Categorylist.js and ProductList.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap';
 function App() {
   return (
     <div>
-      <h2>Hello from React!</h2>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs='3'>
+            <CategoryList />
+          </Col>
+          <Col xs='9'>
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
     </div>
-    
   );
 }
 
